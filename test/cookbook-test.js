27b5eb2e7d07055ba68efe4b7787f013bc764310
Kimala Cochran <kimalajoy@gmail.com>
2020-03-24 15:39:08 -0600

    updated cookbook class and extends into pantry and user

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -1,2 +1,19 @@
 import {expect} from 'chai';
+import recipeData from '../data/recipes.js';
+import ingredientData from '../data/ingredients.js';
+import Cookbook from '../src/Cookbook.js'
 
+let cookbook;
+
+
+describe('Cookbook', () => {
+  beforeEach(() => {
+    cookbook = new Cookbook();
+  });
+
+
+})
+
+
+
+// Cookbook - calculateCost and findRecipe
