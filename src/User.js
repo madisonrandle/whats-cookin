28d077e19b771f1b405cbe7f188b1cc5fce67720
Madison Randle <madisonrandle25@gmail.com>
2020-03-25 17:07:59 -0600

    Add method to add recipes to favoriteRecipes array

--- src/User.js
+++ src/User.js
@@ -1,7 +1,15 @@
-class User extends Cookbook {
-  constructor (ingredientsData, recipesData, userData) {super (ingredientsData, recipesData)
+class User {
+  constructor (userData, favoriteRecipes) {
     this.userData = userData;
+    this.favoriteRecipes = [];
   }
+
+  // add to / remove from the user’s favoriteRecipes
+  addToFavorites(recipe) {
+    !this.favoriteRecipes.includes(recipe) && this.favoriteRecipes.push(recipe);
+  }
+
+
 }
 
 if (typeof module !== 'undefined') {
