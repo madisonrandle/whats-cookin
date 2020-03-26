61ea08b5b30d536403c83d6e8eb053d073997bc4
Madison Randle <madisonrandle25@gmail.com>
2020-03-25 21:33:16 -0600

    Add method to toggle active button image

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,12 @@
 
 
+let body = document.querySelector('body');
 let userWelcomeMessage = document.querySelector('.title-and-greeting-wrapper');
+let subheaderWrapper = document.querySelector('.my-recipes');
 let recipesWrapper = document.querySelector('main');
 
-let user;
 
+let user;
 
 const findRandomUser = () => {
   for (let i = usersData.length - 1; i > 0; i--) {
@@ -16,7 +18,8 @@ const findRandomUser = () => {
 
 const getUserData = () => {
   let userData = findRandomUser();
-  // user = new User(userData);
+  user = new User(userData);
+  displayRecipeCards();
   displayUserName(userData);
 }
 
@@ -28,13 +31,13 @@ const displayUserName = (userData) => {
 };
 
 const displayRecipeCards = () => {
-  getUserData();
+  recipesWrapper.innerHTML = '';
   recipeData.forEach(recipe => {
     recipesWrapper.insertAdjacentHTML('afterbegin', `
       <article class="recipe-card">
         <section class="recipe-card-header">
           <button tabindex="2" type="button" class="add-recipe-icon"></button>
-          <button tabindex="2" type="button" class="favorite-recipe-icon-inactive"></button>
+          <button tabindex="2" type="button" class="favorite-recipe-icon-inactive favorite-recipe-icon-active"></button>
         </section>
         <p class="recipe-name">${recipe.name}</p>
         <section tabindex="2" class="recipe-card-main">
@@ -45,6 +48,49 @@ const displayRecipeCards = () => {
   });
 };
 
-displayRecipeCards();
+const displayFavoriteRecipes = () => {
+  subheaderWrapper.innerHTML = '';
+  recipesWrapper.innerHTML = '';
+  subheaderWrapper.insertAdjacentHTML('afterbegin', `
+      <h1>My Recipes</h1>
+      <div class="my-recipes-nav-buttons-wrapper">
+        <button tabindex="2" class="all-recipes-button" type="button">All Recipes</button>
+      </div>
+  `);
+}
+
+const displayRecipesToCook = () => {
+  subheaderWrapper.innerHTML = '';
+  recipesWrapper.innerHTML = '';
+  subheaderWrapper.insertAdjacentHTML('afterbegin', `
+      <h1>This Week</h1>
+      <div class="my-recipes-nav-buttons-wrapper">
+        <button tabindex="2" class="all-recipes-button" type="button">All Recipes</button>
+      </div>
+  `);
+}
+
+const displayActiveFavoriteButton = (e) => {
+  e.target.classList.toggle('favorite-recipe-icon-inactive');
+
+
+
+}
+
+getUserData();
+
+const eventHandler = (e) => {
+  if (e.target.classList.contains('view-favorites-button')) {
+    displayFavoriteRecipes(e);
+  } else if (e.target.classList.contains('this-week-button')) {
+    displayRecipesToCook(e);
+  } else if (e.target.classList.contains('home-button')) {
+    displayRecipeCards(e);
+  } else if (e.target.classList.contains('favorite-recipe-icon-active')) {
+    displayActiveFavoriteButton(e);
+  }
+
+}
+body.addEventListener('click', eventHandler);
 
 //
