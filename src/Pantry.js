1d21d97bfdc1cc12bda8cde4a51cc1ad78151ceb
Kimala Cochran <kimalajoy@gmail.com>
2020-03-26 22:11:30 -0600

    added methods and tests to pantry

--- src/Pantry.js
+++ src/Pantry.js
@@ -1,7 +1,42 @@
-class Pantry extends Cookbook {
-  constructor (ingredientsData, recipesData, userPantry) {
-    super (ingredientsData, recipesData)
-    this.userPantry = userPantry;
+class Pantry {
+  constructor(userPantry, ingredientsData) {
+    this.pantry = userPantry;
+    this.ingredientsData = ingredientsData;
+  }
+
+  canUserPantryCookSelectedMeal(recipe) {
+    let response = {
+      success: true,
+      missing: []
+    };
+
+    recipe.ingredients.forEach(recipeIngredient=> {
+      let userIngredient = this.pantry.find(i => recipeIngredient.id === i.ingredient);
+
+      if (!userIngredient) {
+        response.success = false;
+        response.missing.push({
+          id: recipeIngredient.id,
+          name: this.getIngredientNameById(recipeIngredient.id),
+          amount: recipeIngredient.quantity.amount
+        });
+      } else if (userIngredient.amount < recipeIngredient.quantity.amount) {
+        response.success = false;
+        response.missing.push({
+          id: recipeIngredient.id,
+          name: this.getIngredientNameById(recipeIngredient.id),
+          amount: recipeIngredient.quantity.amount - userIngredient.amount
+        });
+      }
+    });
+
+    return response;
+  }
+
+  getIngredientNameById(id) {
+    let i = this.ingredientsData.find(name => name.id === id);
+    
+    return (!i) ? '' : i.name;
   }
 }
 
