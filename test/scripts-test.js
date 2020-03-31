f3342a71651c833aa1d65931549df1b0969fa823
Madison Randle <madisonrandle25@gmail.com>
2020-03-30 23:22:18 -0600

    Add skeleton for scripts test

---
+++ test/scripts-test.js
@@ -0,0 +1,5 @@
+const chai = require('chai');
+const expect = chai.expect;
+const User = require('../src/User.js');
+const usersData = require('../data/users.js');
+const recipesData = require('../data/recipes.js');
